5b32d33e687bcd08853e4f45a6999e0cf65e3823
ds1242 <david.shaw1242@gmail.com>
2022-10-11 14:14:03 -0600

    refactored snake into it an object to simplify main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
+from snake import Snake
 
 
 screen = Screen()
@@ -8,17 +9,7 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
-
-
-for position in starting_positions:
-    new_segement = Turtle("square")
-    new_segement.color("white")
-    new_segement.penup()
-    new_segement.goto(position)
-    segments.append(new_segement)
+snake = Snake()
 
 screen.update()
 game_is_on = True
@@ -26,11 +17,9 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
+
+
 
 
 
