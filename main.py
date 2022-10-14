42897fbc201aebb7d6f353a7dc77408cd17d1f42
ds1242 <david.shaw1242@gmail.com>
2022-10-14 09:54:18 -0600

    created a scoreboard class and functions required to make the scoreboard function correctly

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from turtle import Screen, Turtle
 import time
 from snake import Snake
 from food import Food
-
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,26 +28,11 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # detect collision of food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen.exitonclick()
